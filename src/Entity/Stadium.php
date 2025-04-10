1130241ba70a233726f8026a278de4305ba6f47c
simon.hrdy <simon.hrdy@geekcoders.cz>
2025-04-10 12:53:46 +0200

    searchda

--- src/Entity/Stadium.php
+++ src/Entity/Stadium.php
@@ -12,15 +12,15 @@ class Stadium
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['list:list', 'team:list', 'game:list'])]
+    #[Groups(['list:list', 'team:list', 'game:list', 'player:list'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['list:list', 'team:list', 'game:list'])]
+    #[Groups(['list:list', 'team:list', 'game:list', 'player:list'])]
     private ?string $name = null;
 
     #[ORM\Column(nullable: true)]
-    #[Groups(['list:list', 'team:list', 'game:list'])]
+    #[Groups(['list:list', 'team:list', 'game:list', 'player:list'])]
     private ?int $capacity = null;
 
     public function getId(): ?int
